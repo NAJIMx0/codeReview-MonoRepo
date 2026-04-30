777fed4e59592d5e631c01b79a991f7828dd6938
NAJIMx0 <sotanajim007@gmail.com>
2026-04-30 16:11:39 +0100

    remove large file

--- services/webhook-service/src/main/java/com/najim/webhookservice/WebhookServiceApplication.java
+++ services/webhook-service/src/main/java/com/najim/webhookservice/WebhookServiceApplication.java
@@ -2,8 +2,10 @@ package com.najim.webhookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WebhookServiceApplication {
 
     public static void main(String[] args) {
--- services/webhook-service/src/main/java/com/najim/webhookservice/controller/webhookController.java
+++ services/webhook-service/src/main/java/com/najim/webhookservice/controller/webhookController.java
@@ -1,4 +1,26 @@
 package com.najim.webhookservice.controller;
 
+import com.najim.webhookservice.service.webhookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
 public class webhookController {
+
+   private  webhookService webhookService;
+
+   @PostMapping("/api/webhook/github")
+    public ResponseEntity<Map<String,String>> githubWebhook(
+
+   ){
+       return ResponseEntity.ok(Map.of("status", "received"));
+   }
+
 }
